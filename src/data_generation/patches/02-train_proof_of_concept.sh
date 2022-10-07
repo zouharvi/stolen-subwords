9da97d8b52eda963681cc8a53e155fe40f5eba7b
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-10-07 22:22:48 +0200

    submit teacher translate

--- src/data_generation/patches/02-train_proof_of_concept.sh
+++ src/data_generation/patches/02-train_proof_of_concept.sh
@@ -19,7 +19,12 @@ cat ~/.sacrebleu/wmt21/wmt21.en-de.src
 
 # teacher translate
 for ((i=0; i < 10; i++)); do 
-    bsub -n 4 -R "rusage[mem=4096,ngpus_excl_p=1]" \
+    bsub -W 20:00 -n 4 -R "rusage[mem=4096,ngpus_excl_p=1]" \
         ./src/teacher_translate.py \
         -i data/word_3gram_en.${i}.txt -o data/word_3gram_ende.${i}.txt ;
 done
+
+# translate WMT21 test set 
+bsub -W 4:00 -n 4 -R "rusage[mem=4096,ngpus_excl_p=1]" \
+    ./src/teacher_translate.py \
+    -i /cluster/home/vzouhar/.sacrebleu/wmt21/wmt21.en-de.src -o data/wmt21.en-de.teacher-tgt
