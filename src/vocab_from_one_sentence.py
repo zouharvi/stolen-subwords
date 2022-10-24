05aa4cba53adeb4c56cc73d2b80dc17c5453d5b0
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-10-24 14:46:14 +0200

    sanitize vocab items

--- src/vocab_from_one_sentence.py
+++ src/vocab_from_one_sentence.py
@@ -34,26 +34,6 @@ model_a.to("cuda")
 model_b.eval()
 model_b.to("cuda")
 
-
-# def cycle_translate(sentA):
-#     print()
-#     for i in range(1, 10):
-#         print(f"{i}a ###", sentA)
-#         sentB = model_a.translate(sentA)
-#         print(f"{i}b ###", sentB)
-#         sentA = model_b.translate(sentB)
-
-# # char zerogram 8
-# cycle_translate("TzpEyVsCJ EqTLp  W sR PCgAK  CZpWGaRxXOaD ot lIo WAetE gHQG")
-# # char unigram en
-# cycle_translate(
-#     "P eniirmatugh)whe snd)we.eamIhvtrar)ostov dtiscehesArre  Sc5itodp tnol iect0o s1himEtm) or ia een 1oeu cno. n .rrs")
-# # word unigram en
-# cycle_translate("alof will achefficient At failure massive as . such to issufootfutviolence satorganisations within important inappropriate ails lyof first meare across eit the ) them")
-# # word bigram en
-# cycle_translate(
-#     "in as in following dossier violent annum the . When impact are can gender ( of application for Action encouraging report the")
-
 def get_translation(model, vocab_freq):
     # keep how many times a word was sampled from a vocab and set selection weight to 1/k
     # sent_src = " ".join(random.choices(list(vocab), k=20))
@@ -68,11 +48,26 @@ def get_translation(model, vocab_freq):
         sent_src.append(w)
     sent_src = " ".join(sent_src)
     sent_tgt = model.translate(sent_src)
-    return set(sent_tgt.split())
+    return sent_tgt
 
 def dump_vocab(path, vocab):
     with open(path, "w") as f:
         f.write("\n".join(vocab))
+
+def add_to_vocab(vocab, vocab_freq, sent):
+    extended = False
+    sent = set(sent.split())
+    for w in sent:
+        # sanitize words
+        w = ''.join(c for c in w.lower() if c.isalnum())
+        if len(w) > 30:
+            continue
+        if w not in vocab:
+            extended = True
+            vocab.add(w)
+            vocab_freq.append([w, 1])
+    return extended
+
 
 SENT_A = "In model functionality stealing, we aim to replicate the victim model MV functionality to which we have either completely blackbox or grey-box (logits)."
 vocab_a = set(SENT_A.split())
@@ -80,6 +75,8 @@ vocab_a_freq = [[w, 1] for w in vocab_a]
 vocab_b = set()
 vocab_b_freq = []
 
+PATIENCE_MAX = 3
+patience = PATIENCE_MAX
 for i in range(10000):
     if i < 10 or i % 10 == 0:
         print(f"{i:>5}, EN: {len(vocab_a):>5}, DE: {len(vocab_b):>5}")
@@ -87,22 +84,18 @@ for i in range(10000):
         dump_vocab(args.vocab_b, vocab_b)
 
     # extend B
-    vocab_b_ext = get_translation(model_a, vocab_a_freq)
-    extended_b = False
-    for w in vocab_b_ext:
-        if w not in vocab_b:
-            extended_b = True
-            vocab_b.add(w)
-            vocab_b_freq.append([w, 1])
+    sent_b = get_translation(model_a, vocab_a_freq)
+    extended_b = add_to_vocab(vocab_b, vocab_b_freq, sent_b)
 
     # extend A
-    vocab_a_ext = get_translation(model_b, vocab_b_freq)
-    extended_a = False
-    for w in vocab_a_ext:
-        if w not in vocab_a:
-            extended_a = True
-            vocab_a.add(w)
-            vocab_a_freq.append([w, 1])
+    sent_a = get_translation(model_b, vocab_b_freq)
+    extended_a = add_to_vocab(vocab_a, vocab_a_freq, sent_a)
 
     if (not extended_a) and (not extended_b):
+        patience -= 1
+        print("DECREASING PATIENCE TO", patience)
+    else:
+        patience = PATIENCE_MAX
+
+    if patience == 0:
         break
